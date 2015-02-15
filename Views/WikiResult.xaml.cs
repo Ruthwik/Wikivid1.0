b2ebec569f096be6c2cada77cd8c7387610a3129
Rachit Magon <rachit.magon@gmail.com>
2015-02-15 09:29:02 -0500

    Inner Search Box
    Added Events to the inner search box

--- Views/WikiResult.xaml.cs
+++ Views/WikiResult.xaml.cs
@@ -15,6 +15,11 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Wikivid1._0.ViewModel;
 using Wikivid1._0.Model;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Search;
+using System.Net.Http;
+using Windows.Data.Json;
+using Windows.Storage.Streams;
 
 // The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
 
@@ -52,7 +57,7 @@ namespace Wikivid1._0.Views
         {
             string html;
             this.InitializeComponent();
-            
+            httpClient = new HttpClient();
             this.navigationHelper = new NavigationHelper(this);
             this.navigationHelper.LoadState += navigationHelper_LoadState;
             this.navigationHelper.SaveState += navigationHelper_SaveState;
@@ -228,6 +233,7 @@ namespace Wikivid1._0.Views
             }
         }
 
+        #region DISPATCHER
 
         DispatcherTimer dispatcherTimer;
         DateTimeOffset startTime;
@@ -252,7 +258,10 @@ namespace Wikivid1._0.Views
             TimeSpan span = time - lastTime;
             lastTime = time;
             timesTicked++;
-            ProgressText.Text = funnyText[timesTicked%15];
+            Random R = new Random();
+            ;
+            ProgressText.Text = funnyText[R.Next(0, 19)];
+            
         }
 
         string []funnyText = {"Loading Please Wait..",
@@ -271,6 +280,132 @@ namespace Wikivid1._0.Views
                              "Word Hard. Dream Big",
                              "The Machine is Learning",
                              "Getting Videos...",
-                             "Getting some nice Text"};
+                             "Getting some nice Text",
+                             "It's something wait the worth!",
+    "Java Developers wear glasses because they can't C#","Applying some Data Mining","","",""};
+
+        #endregion
+
+        #region SEARCHBOX
+        private HttpClient httpClient;
+        private Task<string> currentHttpTask;
+        /// <summary>
+        /// Completes the retreval of suggestions from the web service
+        /// </summary>
+        /// <param name="str">User query string</param>
+        /// <param name="suggestions">Suggestions list to append new suggestions</param>
+        /// <returns></returns>
+        private async Task GetSuggestionsAsync(string str, SearchSuggestionCollection suggestions)
+        {
+            // Cancel the previous suggestion request if it is not finished.
+            if (currentHttpTask != null)
+            {
+                currentHttpTask.AsAsyncOperation<string>().Cancel();
+            }
+
+            // Get the suggestions from an open search service.
+            currentHttpTask = httpClient.GetStringAsync(str);
+            string response = await currentHttpTask;
+            JsonArray parsedResponse = JsonArray.Parse(response);
+            if (parsedResponse.Count > 1)
+            {
+                suggestions.AppendSearchSeparator("Exact Match");
+                foreach (JsonValue value in parsedResponse[1].GetArray())
+                {
+                    //suggestions.AppendQuerySuggestion(value.GetString());
+                    var imageUri = new Uri("ms-appx:///test.png");
+                    var imageRef = RandomAccessStreamReference.CreateFromUri(imageUri);
+                    suggestions.AppendResultSuggestion(value.GetString(), "", value.GetString(), imageRef, "");
+                    //suggestions.AppendResultSuggestion(value.GetString(), "Details", "baz", imageRef, "Result");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Populates SearchBox with Suggestions when user enters text
+        /// </summary>
+        /// <param name="sender">Xaml SearchBox</param>
+        /// <param name="e">Event when user changes text in SearchBox</param>
+        private async void SearchBoxEventsSuggestionsRequested(Object sender, SearchBoxSuggestionsRequestedEventArgs e)
+        {
+            var queryText = e.QueryText;
+            string url = "";
+            if (string.IsNullOrEmpty(queryText))
+            {
+                //tbError.Text = "Use the search control to submit a query";
+                //MainPage.Current.NotifyUser("Use the search control to submit a query", NotifyType.StatusMessage);
+            }
+            else
+            {
+                // The deferral object is used to supply suggestions asynchronously for example when fetching suggestions from a web service.
+                var request = e.Request;
+                var deferral = request.GetDeferral();
+
+                try
+                {
+                    url = @"http://en.wikipedia.org/w/api.php?action=opensearch&search=" + queryText + @"&limit=10&namespace=0&format=json";
+                    // Use the web service Url entered in the UrlTextBox that supports OpenSearch Suggestions in order to see suggestions come from the web service.
+                    // See http://www.opensearch.org/Specifications/OpenSearch/Extensions/Suggestions/1.0 for details on OpenSearch Suggestions format.
+                    // Replace "{searchTerms}" of the Url with the query string.
+                    Task task = GetSuggestionsAsync(url, request.SearchSuggestionCollection);
+                    await task;
+
+                    if (task.Status == TaskStatus.RanToCompletion)
+                    {
+                        if (request.SearchSuggestionCollection.Size > 0)
+                        {
+
+                            //MainPage.Current.NotifyUser("Suggestions provided for query: " + queryText, NotifyType.StatusMessage);
+                        }
+                        else
+                        {
+                            // MainPage.Current.NotifyUser("No suggestions provided for query: " + queryText, NotifyType.StatusMessage);
+                        }
+                    }
+                }
+                catch (TaskCanceledException)
+                {
+                    // We have canceled the task.
+                }
+                catch (FormatException)
+                {
+                   // tbError.Text = "Suggestions could not be retrieved -- please verify that the URL points to a valid service";
+                    //  MainPage.Current.NotifyUser(@"Suggestions could not be retrieved -- please verify that the URL points to a valid service (for example http://contoso.com?q={searchTerms}", NotifyType.ErrorMessage);
+                }
+                catch (Exception es)
+                {
+                   // tbError.Text = "Some Random Excpetion " + es.StackTrace;
+                    // MainPage.Current.NotifyUser("Suggestions could not be displayed -- please verify that the service provides valid OpenSearch suggestions", NotifyType.ErrorMessage);
+                }
+                finally
+                {
+                    deferral.Complete();
+                }
+            }
+        }
+
+        private void SearchBoxSuggestions_ResultSuggestionChosen(SearchBox sender, SearchBoxResultSuggestionChosenEventArgs args)
+        {
+            string chosen = args.Tag.ToString();
+            SearchQuery sq = new SearchQuery()
+            {
+                Query = chosen,
+                ExaxtMatch = 1
+            };
+            this.Frame.Navigate(typeof(Views.WikiResult), sq);
+        }
+
+        private void SearchBoxSuggestions_QuerySubmitted(SearchBox sender, SearchBoxQuerySubmittedEventArgs args)
+        {
+            string chosen = args.QueryText;
+            SearchQuery sq = new SearchQuery()
+            {
+                Query = chosen,
+                ExaxtMatch = 0
+            };
+            this.Frame.Navigate(typeof(Views.WikiResult), sq);
+        }
+
+        #endregion
     }
 }
